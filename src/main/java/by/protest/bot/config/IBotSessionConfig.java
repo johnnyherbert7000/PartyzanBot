dbb5e355f17f713ae28f09779206bafe06708312
johnnyherbert7000@gmail.com <johnnyherbert7000@gmail.com>
2022-02-10 02:05:57 +0300

    add data and amend session time

--- src/main/java/by/protest/bot/config/IBotSessionConfig.java
+++ src/main/java/by/protest/bot/config/IBotSessionConfig.java
@@ -4,8 +4,8 @@ public interface IBotSessionConfig {
 	static final int SENDER_SLEEP_TIME = 1000;
 	static final int WAIT_FOR_NEW_MESSAGE_DELAY = 1000;
 	//static final String BOT_SESSION_CRON = "30 * * * * ?";
-	static final String BOT_SESSION_CRON = "* 0/5 * * * ?";
-	//static final String BOT_SESSION_CRON = "* 30 * * * ?";
+	//static final String BOT_SESSION_CRON = "* 0/5 * * * ?";
+	static final String BOT_SESSION_CRON = "* 30 * * * ?";
 	
 	static final long RECONNECT_PAUSE = 10000L;
 }
